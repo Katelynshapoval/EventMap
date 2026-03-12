0d29769cf2d5ce475cd46466b203baf12f35335a
Kateryna Shapoval <katelyn.shapoval@gmail.com>
2026-03-12 12:07:36 +0100

    accidents uk data

--- main.py
+++ main.py
@@ -1,38 +1,31 @@
-import pydeck as pdk
+import pydeck
 
-data = {
-    "type": "FeatureCollection",
-    "features": [
-        {
-            "type": "Feature",
-            "geometry": {
-                "type": "Polygon",
-                "coordinates": [[
-                    [-0.89, 41.64],
-                    [-0.88, 41.64],
-                    [-0.88, 41.65],
-                    [-0.89, 41.65],
-                    [-0.89, 41.64]
-                ]]
-            }
-        }
-    ]
-}
+# 2014 locations of car accidents in the UK
+UK_ACCIDENTS_DATA = ('https://raw.githubusercontent.com/uber-common/'
+                     'deck.gl-data/master/examples/3d-heatmap/heatmap-data.csv')
 
-layer = pdk.Layer(
-    "GeoJsonLayer",
-    data,
+# Define a layer to display on a map
+layer = pydeck.Layer(
+    'HexagonLayer',
+    UK_ACCIDENTS_DATA,
+    get_position='[lng, lat]',
+    auto_highlight=True,
+    elevation_scale=50,
+    pickable=True,
+    elevation_range=[0, 3000],
     extruded=True,
-    get_elevation=100,
-    get_fill_color=[255, 0, 0],
-)
+    coverage=1)
 
-view = pdk.ViewState(
-    latitude=41.6488,
-    longitude=-0.8891,
-    zoom=14,
-    pitch=45
-)
+# Set the viewport location
+view_state = pydeck.ViewState(
+    longitude=-1.415,
+    latitude=52.2323,
+    zoom=6,
+    min_zoom=5,
+    max_zoom=15,
+    pitch=40.5,
+    bearing=-27.36)
 
-deck = pdk.Deck(layers=[layer], initial_view_state=view)
-deck.to_html("map.html")
+# Render
+r = pydeck.Deck(layers=[layer], initial_view_state=view_state)
+r.to_html('map.html')
